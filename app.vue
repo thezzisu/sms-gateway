f974cada1d74ad70a93b8a98383c1ff2d87b6ee3
thezzisu <thezzisu@gmail.com>
2023-06-16 21:06:15 +0800

    feat: update api

--- app.vue
+++ app.vue
@@ -1,3 +1,38 @@
 <template>
-  <div>SMS Gateway</div>
+  <div>
+    <h1>SMS Gateway</h1>
+    <input v-model="token" />
+    <input v-model="targets" />
+    <input v-model="type" />
+    <input v-model="content" />
+    <button @click="sendSMS">Send</button>
+  </div>
 </template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const token = ref('')
+const targets = ref('')
+const type = ref('')
+const content = ref('')
+
+function sendSMS() {
+  const data = {
+    targets: targets.value.split(','),
+    type: type.value,
+    content: content.value
+  }
+  fetch('http://localhost:3000/api/send', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'X-API-TOKEN': token.value
+    },
+    body: JSON.stringify(data)
+  })
+    .then((res) => res.json())
+    .then((res) => console.log(res))
+    .catch((err) => console.log(err))
+}
+</script>
--- server/api/send.post.ts
+++ server/api/send.post.ts
@@ -2,23 +2,27 @@ import { Type } from '@sinclair/typebox'
 import { TypeCompiler } from '@sinclair/typebox/compiler'
 import * as tencent from 'tencentcloud-sdk-nodejs-sms'
 
-const SmsClient = tencent.sms.v20210111.Client
-
-const Body = Type.Object({
-  credential: Type.Object({
-    secretId: Type.String(),
-    secretKey: Type.String()
-  }),
-  params: Type.Any()
-})
-
-const BodyChecker = TypeCompiler.Compile(Body)
+const C = TypeCompiler.Compile(
+  Type.Object({
+    targets: Type.Array(Type.String()),
+    type: Type.String(),
+    content: Type.String()
+  })
+)
 
 export default defineEventHandler(async (event) => {
-  const body = await readBody(event)
-  if (!BodyChecker.Check(body)) throw createError({ statusCode: 400 })
-  const client = new SmsClient({
-    credential: body.credential,
+  const config = useRuntimeConfig()
+  console.log(config)
+  const tokens: string[] = config.apiTokens.split(',')
+  const token = getHeader(event, 'X-API-TOKEN')
+  if (!token || !tokens.includes(token)) {
+    throw createError({ statusCode: 401, message: 'Unauthorized' })
+  }
+  const client = new tencent.sms.v20210111.Client({
+    credential: {
+      secretId: config.tencentSecretId,
+      secretKey: config.tencentSecretKey
+    },
     region: 'ap-beijing',
     profile: {
       httpProfile: {
@@ -26,6 +30,17 @@ export default defineEventHandler(async (event) => {
       }
     }
   })
-  const resp = await client.SendSms(body.params)
+  const body = await readBody(event)
+  if (!C.Check(body)) {
+    throw createError({ statusCode: 400, message: 'Bad Request' })
+  }
+  const resp = await client.SendSms({
+    PhoneNumberSet: body.targets,
+    SmsSdkAppId: '' + config.tencentSmsAppId,
+    SignName: '' + config.tencentSmsSign,
+    TemplateId: '' + config.tencentSmsTemplateId,
+    // 你有{1}条{2}消息，校验码{3}
+    TemplateParamSet: ['1', body.type, body.content]
+  })
   return resp
 })
